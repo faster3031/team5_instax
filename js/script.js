38595ab4d053cfbb1a4eeb0407dcedb925ebc13d
김민정 <dst04021@gmail.com>
2023-08-29 16:07:06 +0900

    feat: accessories, banner, detail 영역 작업 중
    accessories, banner, detail 영역 1차 완료

--- js/script.js
+++ js/script.js
@@ -1,5 +1,18 @@
 //  dom (html 태그 로딩 완료시 실행)
-$(document).ready(function () { });
+$(document).ready(function () {
+    /* 슬라이드 */
+    var slide_hero = $('.detail-slide');
+    slide_hero.owlCarousel({
+        items: 3,
+        loop: true,
+        nav: true,
+        center: true,
+        callbacks: true,
+        // autoplay: true,
+        // autoplayTimeout: 5000,
+        // autoplayHoverPause: false,
+    });
+});
 // 멀티미디어 리소스 로딩 완료 후 실행
 window.onload = function(){
     
