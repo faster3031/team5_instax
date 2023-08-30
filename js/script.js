3d5ae9514bbd51d87c9d24d36dd5da28748a11b5
김민정 <dst04021@gmail.com>
2023-08-30 16:36:03 +0900

    feat: movie영역, detail 영역 수정
    movie 영역 비디오 shadow 추가
    detail 영역 hover js 추가

--- js/script.js
+++ js/script.js
@@ -1,139 +1,149 @@
 //  dom (html 태그 로딩 완료시 실행)
 $(document).ready(function () {
-    // 클릭시 점멸
-    function flash(e) {
-        $(".flash")
-        .show() //show the hidden div
-        .animate({ opacity: 1 }, 100)
-        .fadeOut(500)
-        .css({ opacity: 0.5 });
-    }
-    $(".flash").hide();
-    $(".visual-camera").click(function (e) {
-        flash(e);
-    });
+  // 클릭시 점멸
+  function flash(e) {
+    $(".flash")
+      .show() //show the hidden div
+      .animate({ opacity: 1 }, 100)
+      .fadeOut(500)
+      .css({ opacity: 0.5 });
+  }
+  $(".flash").hide();
+  $(".visual-camera").click(function (e) {
+    flash(e);
+  });
 
-    // 각 섹션의 위치값(세로스크롤 위치)
-    const sectionYpos = [1000, 2000, 3000, 4000, 5000];
-    // 클래스 nav 의 li 를 찾아라
-    // 저장한다. 재활용하기 위해서
-    const navLis = $(".nav ul li");
-    // li 에 a 태그를 클릭을 해서 스크롤을 이동
-    const navLisA = $(".nav ul li a");
-    // 클릭 기능 구현
-    $.each(navLisA, function (index, item) {
-      // item 은 a 태그를 말합니다.
-      // item 을 클릭을 할 겁니다.
-      // item 은 html 태그 (jQuery 용도)
-        $(this).click(function (event) {
-            // a 태그의 href 막기
-            event.preventDefault();
-            // li 의 모든 클래스 제거
-            navLis.removeClass("focus-active");
-            // 클릭된 li 에 focus-active 추가하기
-            navLis.eq(index).addClass("focus-active");
-            // 2. 클릭하면 스크롤바가 움직인다.
-            $("html, body").animate(
-            {scrollTop: sectionYpos[index],},
-            "slow"
-            );
-        });
+  // 각 섹션의 위치값(세로스크롤 위치)
+  const sectionYpos = [1000, 2000, 3000, 4000, 5000];
+  // 클래스 nav 의 li 를 찾아라
+  // 저장한다. 재활용하기 위해서
+  const navLis = $(".nav ul li");
+  // li 에 a 태그를 클릭을 해서 스크롤을 이동
+  const navLisA = $(".nav ul li a");
+  // 클릭 기능 구현
+  $.each(navLisA, function (index, item) {
+    // item 은 a 태그를 말합니다.
+    // item 을 클릭을 할 겁니다.
+    // item 은 html 태그 (jQuery 용도)
+    $(this).click(function (event) {
+      // a 태그의 href 막기
+      event.preventDefault();
+      // li 의 모든 클래스 제거
+      navLis.removeClass("focus-active");
+      // 클릭된 li 에 focus-active 추가하기
+      navLis.eq(index).addClass("focus-active");
+      // 2. 클릭하면 스크롤바가 움직인다.
+      $("html, body").animate({ scrollTop: sectionYpos[index] }, "slow");
     });
+  });
 
-    /* 슬라이드 */
-    var slide_hero = $('.detail-slide');
-    slide_hero.owlCarousel({
-        items: 3,
-        loop: true,
-        nav: true,
-        center: true,
-        callbacks: true,
-        // autoplay: true,
-        // autoplayTimeout: 5000,
-        // autoplayHoverPause: false,
+  /* 슬라이드 */
+  var slide_hero = $(".detail-slide");
+  slide_hero.owlCarousel({
+    items: 3,
+    loop: true,
+    nav: true,
+    center: true,
+    callbacks: true,
+    // autoplay: true,
+    // autoplayTimeout: 5000,
+    // autoplayHoverPause: false,
+  });
+
+  // detail-slide 마우스 오버 효과
+  const items = slide_hero.find(".item");
+  $.each(items, function (index, item) {
+    // 큰 이미지
+    const bigPics = $(item).find(".detail-pic img");
+    // 색상버튼들
+    const colorBalls = $(item).find(".detail-color > div");
+    $.each(colorBalls, function (index2, item2) {
+      $(item2).mouseenter(function () {
+        bigPics.hide();
+        $(bigPics[index2]).show();
+      });
     });
+  });
 });
 
 // 멀티미디어 리소스 로딩 완료 후 실행
-window.onload = function(){
-    
-};
+window.onload = function () {};
 
 // swiper
 window.addEventListener("load", function () {
-    const swiper = new Swiper(".sample-slider", {
-        loop: true,
-        speed: 1000,
-        slidesPerView: 5,
-        spaceBetween: 150,
-        autoplay: {delay: 0},
-    });
-    
-    // top 버튼 스크롤 기능
-    const topBtn = document.getElementById("top-btn");
-    topBtn.addEventListener("click", function (event) {
-        event.preventDefault();
-        console.log(window.scrollY);
-        if (window.scrollY == 0) {
-            window.scrollTo({
-            top: 99999,
-            behavior: "smooth",
-            });
-        } else {
-            window.scrollTo({
-                top: 0,
-                behavior: "smooth",
-            });
-        }
-    });
-    // 화살표 이미지 회전
-    const topBtnImg = document.getElementById("top-btn-img");
-    window.addEventListener("scroll", function (scTop) {
-        scTop = this.document.documentElement.scrollTop;
-        if (scTop > 0) {
-            topBtnImg.classList.add("up");
-        } else {
-            topBtnImg.classList.remove("up");
-        }
-    });
+  const swiper = new Swiper(".sample-slider", {
+    loop: true,
+    speed: 1000,
+    slidesPerView: 5,
+    spaceBetween: 150,
+    autoplay: { delay: 0 },
+  });
 
-    //헤더 스크롤 기능
-    //스크롤바의 상단위치
-    let scy = 0;
-    let scActive = 50;
-    scy = window.document.documentElement.scrollTop;
-    let header = document.querySelector(".header");
-    header.addEventListener("mouseenter", function () {
-        header.classList.add("header-active");
-    });
-    header.addEventListener("mouseleave", function () {
-        if (scy < scActive) {
-            header.classList.remove("header-active");
-        }
-    });
-    //새로고침 시 적용
-    if (scy > scActive) {
-        header.classList.add("header-active");
+  // top 버튼 스크롤 기능
+  const topBtn = document.getElementById("top-btn");
+  topBtn.addEventListener("click", function (event) {
+    event.preventDefault();
+    console.log(window.scrollY);
+    if (window.scrollY == 0) {
+      window.scrollTo({
+        top: 99999,
+        behavior: "smooth",
+      });
+    } else {
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
     }
-    window.addEventListener("scroll", function () {
-        scy = window.document.documentElement.scrollTop;
-        // console.log(scy);
-        if (scy > scActive) {
-            header.classList.add("header-active");
-        } else {
-            header.classList.remove("header-active");
-        }
-    });
-  
-    let scb = 300;
-    let scr = 400;
-    let movie = document.querySelector(".movie");
-    window.addEventListener("scroll", function () {
-        scb = window.document.documentElement.scrollTop;
-        if (scb > scr) {
-            movie.classList.add("movie-active");
-        } else {
-            movie.classList.remove("movie-active");
-        }
-    });
+  });
+  // 화살표 이미지 회전
+  const topBtnImg = document.getElementById("top-btn-img");
+  window.addEventListener("scroll", function (scTop) {
+    scTop = this.document.documentElement.scrollTop;
+    if (scTop > 0) {
+      topBtnImg.classList.add("up");
+    } else {
+      topBtnImg.classList.remove("up");
+    }
+  });
+
+  //헤더 스크롤 기능
+  //스크롤바의 상단위치
+  let scy = 0;
+  let scActive = 50;
+  scy = window.document.documentElement.scrollTop;
+  let header = document.querySelector(".header");
+  header.addEventListener("mouseenter", function () {
+    header.classList.add("header-active");
+  });
+  header.addEventListener("mouseleave", function () {
+    if (scy < scActive) {
+      header.classList.remove("header-active");
+    }
+  });
+  //새로고침 시 적용
+  if (scy > scActive) {
+    header.classList.add("header-active");
+  }
+  window.addEventListener("scroll", function () {
+    scy = window.document.documentElement.scrollTop;
+    // console.log(scy);
+    if (scy > scActive) {
+      header.classList.add("header-active");
+    } else {
+      header.classList.remove("header-active");
+    }
+  });
+
+  let scb = 300;
+  let scr = 400;
+  let movie = document.querySelector(".movie");
+  window.addEventListener("scroll", function () {
+    scb = window.document.documentElement.scrollTop;
+    if (scb > scr) {
+      movie.classList.add("movie-active");
+    } else {
+      movie.classList.remove("movie-active");
+    }
+  });
 });
